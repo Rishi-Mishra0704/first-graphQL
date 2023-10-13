69b2f6da2feb9b7c8325c2550e0cd057a06375d4
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-13 20:42:00 +0530

    Add resolvers for single query

--- index.js
+++ index.js
@@ -6,33 +6,38 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema.js";
 import db from "./_db.js";
 
-
 // resolvers
 const resolvers = {
-    Query: {
-        games() {
-            return db.games;
-        },
-        reviews() {
-            return db.reviews;
-        },
-        authors() {
-            return db.authors;
-        },
+  Query: {
+    games() {
+      return db.games;
     },
-
-}
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+  },
+};
 // apollo server
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 });
-
 
 // start server
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
-
 });
 
 console.log(`🚀 Server ready at ${url}`);
